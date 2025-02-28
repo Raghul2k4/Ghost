b95b7626820943f5d46b2d2e9b29e95d031f53f8
Raghul <121763518+Raghul2k4@users.noreply.github.com>
2025-02-28 18:35:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Main from "./main";  // Corrected import
+import Main from "./main";
 import AboutUs from "./pages/AboutUs";
 import Categories from "./pages/showTemp";
-// import Wishlist from "./pages/Wishlist";
-// import Liked from "./pages/Liked";
-import Login from "./pages/LogIn"
+import Login from "./pages/LogIn";
 
 function App() {
   return (
-    <>
-      
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/profile" element={<Login/>} />
-        {/* <Route path="/liked" element={<Liked />} /> */}
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/profile" element={<Login />} />
+    </Routes>
   );
 }
 
